22fb61f11d50bbdb90c650ba2de31ddcc008c09f
Ynffr <lasse_94@hotmail.de>
2018-10-02 11:49:50 +0200

    3

--- problem340.py
+++ problem340.py
@@ -15,23 +15,37 @@ import matplotlib.pyplot as plt
 
 A = 50
 B = 2000
-C = 10
+C = 40
 
 #A = 21**7
 #B = 7**21
 #C = 12**7
 
 def F(n : int) -> int:
+    #print("CALL {}".format(n))
+
     if n > B:
-        return n - C
+        r = n - C
     else:
+
         x1 = F(A + n)
+        #print("X1 {}".format(x1))
+
         x2 = F(A + x1)
+        #print("X2 {}".format(x2))
+
         x3 = F(A + x2)
+        #print("X3 {}".format(x3))
+
         x4 = F(A + x3)
+        #print("X4 {}".format(x4))
 
-        return x4
+        r = x4
 
+    #print("RETURN {}".format(r))
+    #print("")
+
+    return r
 
 def S() -> int:
     sum = 0
@@ -41,7 +55,7 @@ def S() -> int:
 
     return sum
 
-x_vals = range(0, 2000)
+x_vals = range(0, 2100)
 y_vals = []
 
 for i in x_vals:
